94a2b0d1006327ef5acd0e2bd1b166cf6d07ad23
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-10-19 12:17:01 -0400

    working server

--- server/app.js
+++ server/app.js
@@ -8,9 +8,11 @@ var path = require('path');
 var dbPath = path.join(__dirname);
 
 db = db.connect(dbPath, ['GlobalStatosphere']);
-app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.json({limit: '5gb'}));       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({   // to support URL-encoded bodies
-  extended: true
+  limit: '5gb',
+  extended: true,
+  parameterLimit: 1000000
 }));
 
 //Receive data from user and send to script to be parsed
@@ -30,6 +32,7 @@ app.post('/rawData', (req, res) => {
 
 //Write to DB to store mission data
 app.post('/parsedData', (req, res) => {
+  console.log("We are now parsing data")
   var mission = {
       name : req.body.name,
       data : req.body.data
@@ -40,7 +43,6 @@ app.post('/parsedData', (req, res) => {
 
 app.get('/visualize', (req, res) => {
   return db.GlobalStatosphere.find({id: req.query.id});
-  return res.send('Received a GET HTTP method');
 });
 
 app.get('/allDataSets', (req, res) => {
@@ -53,3 +55,5 @@ app.get('/allDataSets', (req, res) => {
   })
   return res.send('GET HTTP method on user resource');
 });
+
+app.listen(5050, () => console.log('Example app listening on port 5050!'))
