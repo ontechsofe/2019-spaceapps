23cb4cc69fcaf7265b6ccd193fbb5d7c886b6416
ethanelliott <ethan.elliott@hotmail.ca>
2019-10-19 21:08:06 -0400

    working state

--- stratos/src/views/Main.vue
+++ stratos/src/views/Main.vue
@@ -7,15 +7,19 @@
           <v-col cols="2" class="fill-height">
             <v-sheet color="transparent" class="fill-height pa-5">
               <h1>Altitude</h1>
-              <v-slider :disabled="disabled" readonly :min="0" :max="5000" v-model="altitude" color="white" vertical class="large-slider" thumb-label="always" thumb-color="red"></v-slider>
+              <v-slider :disabled="disabled" readonly :min="0" :max="37000" v-model="altitude" color="white" vertical
+                        class="large-slider" thumb-label="always" thumb-color="red"></v-slider>
             </v-sheet>
           </v-col>
           <v-col cols="7" class="fill-height">
             <v-sheet color="transparent" class="fill-height pa-5">
               <div class="viewer elevation-6">
-                <cesium-viewer :logo="false" :animation="animation" :baseLayerPicker="baseLayerPicker" :camera="camera" :timeline="timeline" :info-box="infoBox" @ready="ready">
+                <cesium-viewer :logo="false" :animation="animation" :baseLayerPicker="baseLayerPicker" :camera="camera"
+                               :timeline="timeline" :info-box="infoBox" @ready="ready">
                   <imagery-layer>
-                    <bingmaps-imagery-provider url="https://dev.virtualearth.net" bmKey="AgcbDCAOb9zMfquaT4Z-MdHX4AsHUNvs7xgdHefEA5myMHxZk87NTNgdLbG90IE-" mapStyle="Aerial"/>
+                    <bingmaps-imagery-provider url="https://dev.virtualearth.net"
+                                               bmKey="AgcbDCAOb9zMfquaT4Z-MdHX4AsHUNvs7xgdHefEA5myMHxZk87NTNgdLbG90IE-"
+                                               mapStyle="Aerial"/>
                   </imagery-layer>
                   <cesium-terrain-provider></cesium-terrain-provider>
                 </cesium-viewer>
@@ -37,7 +41,7 @@
           <v-col cols="12">
             <v-sheet :min="0" :max="5000" color="black" class="pa-5 elevation-6" style="height:100%">
               <h3>Timeline</h3>
-              <v-slider :disabled="disabled" v-model="slider" color="white"></v-slider>
+              <v-slider @change="onTimeChange" thumb-label="always" :min="0" :max="allPoints.length ? allPoints.length : 0" :disabled="disabled" v-model="time" color="white"></v-slider>
             </v-sheet>
           </v-col>
         </v-row>
@@ -45,7 +49,7 @@
     </v-content>
     <v-dialog v-model="dialog" max-width="60%">
       <v-card class="pa-4">
-          <v-img max-height="100%" contain :src="modalImage"></v-img>
+        <v-img max-height="100%" contain :src="modalImage"></v-img>
       </v-card>
     </v-dialog>
   </v-app>
@@ -54,14 +58,15 @@
 <script>
     import CesiumViewer from 'vue-cesium/src/components/viewer/CesiumViewer.vue'
     import Header from '@/components/Header.vue'
+    import axios from 'axios';
 
     export default {
         name: 'main',
         props: ['id'],
         data: () => ({
             disabled: true,
+            time: null,
             altitude: null,
-            slider: null,
             dialog: false,
             horizonSrc: "https://ethanelliott.ca/2019spaceapps/Stratos_DataSet/TIMMINS2018/CDH/CAM2-HOR/100.jpg",
             bottomSrc: "https://ethanelliott.ca/2019spaceapps/Stratos_DataSet/TIMMINS2018/CDH/CAM1-NADIR/103L.jpg",
@@ -69,7 +74,7 @@
             allPoints: [],
             camera: {
                 position: {
-                    lng:  0,
+                    lng: 0,
                     lat: 0,
                     height: 200
                 },
@@ -88,6 +93,11 @@
             CesiumViewer
         },
         methods: {
+            onTimeChange() {
+                if (this.allPoints[this.time]) {
+                    this.altitude = this.allPoints[this.time].altitude;
+                }
+            },
             openHorizonModal() {
                 this.modalImage = this.horizonSrc;
                 this.dialog = true;
@@ -96,21 +106,21 @@
                 this.modalImage = this.bottomSrc;
                 this.dialog = true;
             },
-            ready (cesiumInstance) {
+            ready(cesiumInstance) {
                 this.cesiumInstance = cesiumInstance;
                 const {viewer, Cesium} = this.cesiumInstance;
                 viewer.entities.removeAll();
                 viewer.entities.add({
-                    name : 'Blue dashed line',
-                    polyline : {
-                        positions : Cesium.Cartesian3.fromDegreesArrayHeights([
+                    name: 'Blue dashed line',
+                    polyline: {
+                        positions: Cesium.Cartesian3.fromDegreesArrayHeights([
                             -80.498378, 43.450941, 294,
                             -80.498378, 43.450941, 500,
                             -80.508378, 43.430941, 1000,
                             -80.528378, 43.420941, 294,
                         ]),
-                        width : 4,
-                        material : new Cesium.PolylineDashMaterialProperty({
+                        width: 4,
+                        material: new Cesium.PolylineDashMaterialProperty({
                             color: Cesium.Color.RED
                         })
                     }
@@ -139,19 +149,25 @@
                 let hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
                 let orientation = Cesium.Transforms.headingPitchRollQuaternion(position, hpr);
                 return {
-                    name : "Balloon",
-                    position : position,
-                    orientation : orientation,
-                    model : {
-                        uri : "/SampleData/models/CesiumBalloon/CesiumBalloon.glb",
-                        minimumPixelSize : 64,
-                        maximumScale : 1
+                    name: "Balloon",
+                    position: position,
+                    orientation: orientation,
+                    model: {
+                        uri: "/SampleData/models/CesiumBalloon/CesiumBalloon.glb",
+                        minimumPixelSize: 64,
+                        maximumScale: 1
                     }
                 };
             }
         },
         mounted() {
             console.log(this.id);
+            axios.get(`http://localhost:5050/visualize?id=${this.id}`)
+                .then(res => {
+                    this.allPoints = res.data[0].data;
+                }).catch(err => {
+                console.log(err);
+            })
         }
     }
 </script>
@@ -160,6 +176,7 @@
   .large-slider .v-slider {
     height: 600px;
   }
+
   .viewer {
     width: 100%;
     height: 650px;
